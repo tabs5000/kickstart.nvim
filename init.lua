4ef538f2b4880a0c5691f040a5e6d9d365f75796
Tabs <igor@zibert.dev>
2026-05-24 19:21:36 +0200

    catppuccin

--- init.lua
+++ init.lua
@@ -394,7 +394,7 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'catppuccin'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
